201fbffe945b1f583fd27bd8f06e9593ad3ce871
Lucas Emanuel Lima de Souza <lucasemanuells1995@gmail.com>
2022-08-09 16:03:25 -0300

    Primeiros passos com JS

--- Aula 1/scripts.js
+++ Aula 1/scripts.js
@@ -1,5 +1,5 @@
-let altura, idade, sexo;
-altura = 166;
+let idade, sexo; //Variável que pode ser modificada.
+const altura = 166; //Variável constante que não pode ser modificada.
 idade = 26;
 sexo = 'Masculino';
 console.log(altura);
