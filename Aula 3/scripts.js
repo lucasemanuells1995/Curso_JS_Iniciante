fa479bd9eb21a8c9d55ee95b193d81ebe4effdc2
Lucas Emanuel Lima de Souza <lucasemanuells1995@gmail.com>
2022-08-09 16:15:43 -0300

    Criação de um array

---
+++ Aula 3/scripts.js
@@ -0,0 +1,8 @@
+//Array.
+
+let descricaoAmigo = ['Vitória', 'Cholanda', 26, 'Fortaleza'];
+console.log('Tamanho do Array:', descricaoAmigo.length);
+console.log('Nome:', descricaoAmigo[0]);
+console.log('Sobrenome:', descricaoAmigo[1]);
+console.log('Idade:', descricaoAmigo[2]);
+console.log('Cidade:', descricaoAmigo[3]);
