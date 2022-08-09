212fce72360b5f14fa60c28cbfe726fdff8a89fa
Lucas Emanuel Lima de Souza <lucasemanuells1995@gmail.com>
2022-08-09 16:03:39 -0300

    Criando um objeto

--- Aula 2/scripts.js
+++ Aula 2/scripts.js
@@ -1,10 +1,10 @@
 let nome, sobrenome, idade, nota, media, estaAprovado;
 
-let pessoa = {
+let pessoa = {  //Objeto.
     nome: 'Lucas',
     sobrenome: 'Lima',
     idade: 26,
     nota: 8.5,
     media: 7,
     estaAprovado: true
-}
+} //Da forma anterior pode-se criar um objeto facilmente.
