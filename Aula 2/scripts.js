c78b64ed1e8190fb40d46ba5a3729dca40c8c625
Lucas Emanuel Lima de Souza <lucasemanuells1995@gmail.com>
2022-08-09 15:57:59 -0300

    Criando um objeto

---
+++ Aula 2/scripts.js
@@ -0,0 +1,10 @@
+let nome, sobrenome, idade, nota, media, estaAprovado;
+
+let pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Lima',
+    idade: 26,
+    nota: 8.5,
+    media: 7,
+    estaAprovado: true
+}
