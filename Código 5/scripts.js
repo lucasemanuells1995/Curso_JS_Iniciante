309becb9a13aa7969846bbd6071713f84b2b1dee
Lucas Emanuel Lima de Souza <lucasemanuells1995@gmail.com>
2022-08-22 16:59:16 -0300

    Código FizzBuzz teste

--- Código 5/scripts.js
+++ Código 5/scripts.js
@@ -8,21 +8,21 @@ const resultado = fizzBuzz();
 console.log(resultado);
 
 function fizzBuzz(entrada){
-    if(typeof entrada==='number'){
-        if(entrada%3==0 && entrada%5!=0){
+    if(typeof entrada!=='number'){
+        return 'Não é um número'
+    }
+    else{
+        if((entrada % 3 === 0) && (entrada % 5 === 0)){
+            return 'FizzBuzz'
+        }
+        else if(entrada % 3 === 0){
             return 'Fizz'
         }
-        else if(entrada%5==0 && entrada%3!=0){
+        else if(entrada % 5 === 0){
             return 'Buzz'
-        }
-        else if(entrada%3==0 && entrada%5==0){
-            return 'FizzBuzz'
         }
         else{
             return entrada
         }
-    }
-    else{
-        return 'Não é um número'
     }
 }
