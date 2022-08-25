a483edb9d9ed055bfc1428c0752d6edc4fdbcb07
Lucas Emanuel Lima de Souza <lucasemanuells1995@gmail.com>
2022-08-25 12:32:59 -0300

    Template Literal JS

---
+++ Código 13/scripts.js
@@ -0,0 +1,21 @@
+// Template Literal
+// Object {}
+// Boolean true, false
+// String ''
+// Template Literal = `` (crase)
+
+const nome = 'Rafael';
+const site = 'GitHub';
+const dia = '25/08/2022';
+
+const email =
+`Olá ${nome}!
+
+Fico feliz de te ver acompanhando meu ${site}
+no dia ${dia}. Agradeço, e continue acompanhando
+para ver novos códigos a cada dia!
+
+Atenciosamente,
+Lucas Emanuel.`;
+
+console.log(email);
